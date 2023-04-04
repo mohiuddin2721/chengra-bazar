88786a0d3c8528e1be88e3d62f26a1fb728a685a
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-04-05 04:47:42 +0600

    working on middle button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Notice from './Notice'
 import TopNav from './TopNav'
+import MiddleNav from './MiddleNav'
 
 function Header() {
   return (
     <div>
       <Notice />
       <TopNav />
+      <MiddleNav />
     </div>
   )
 }
