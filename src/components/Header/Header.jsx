bbbca604db652a33f3926d93c287bf4c43a4f7e3
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-04-05 00:13:23 +0600

    all data stored in constantData component and topNav component set bellow the notice

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Notice from './Notice'
+import TopNav from './TopNav'
 
 function Header() {
   return (
     <div>
       <Notice />
+      <TopNav />
     </div>
   )
 }
--- src/components/Header/Notice.jsx
+++ src/components/Header/Notice.jsx
@@ -18,11 +18,10 @@ function Notice() {
     return (
         <div className={`bg-[#240838] duration-500 ${hide && 'hidden'} ${visible ? 'h-auto px-5 py-[10px]' : 'h-0 px-0 py-0'} text-[13px] font-medium tracking-wide leading-[26px] text-white`}>
             <div className='flex justify-center relative'>
-                <div className='flex-wrap flex justify-center items-center gap-2 px-[10px]'>
-                    <span>Get Up to <b>40% OFF</b> New-Season Styles</span>
-                    {/* <button type='button' className={`bg-[#240838] ${!visible && 'hidden'} hover:bg-opacity-50 duration-300 px-2 py-[3px] leading-5 text-[10px] font-sans font-bold`}>MEN</button>
-                    <button type='button' className={`bg-[#240838] ${!visible && 'hidden'} hover:bg-opacity-50 duration-300 px-2 py-[3px] leading-5 text-[10px] font-sans font-bold`}>WOMEN</button> */}
-                    <small className='font-normal opacity-50 md:hidden'>* Limited time only.</small>
+                <div className='flex-wrap flex justify-center items-center gap-2 px-[5px]'>
+                    <span>Get Up to <b>50% OFF</b> New-Season Styles</span>
+                    <button type='button' className={`bg-[#240838] ${!visible && 'hidden'} hover:bg-opacity-50 duration-300 px-2 py-[3px] leading-5 text-[10px] font-sans font-bold`}>Buy now</button>
+                    <small className='font-normal opacity-80 hidden md:flex'>* Limited time only.</small>
                 </div>
                 <TfiClose onClick={() => {
                     setVisible(false);
