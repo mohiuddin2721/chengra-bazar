8af8bd01ba5a20bbe27d39a86661b091a9154f23
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-04-07 21:02:17 +0600

    top slider updated

---
+++ src/Styles/ComponentStyle.jsx
@@ -0,0 +1,23 @@
+import { Typography } from "@mui/material"
+
+export const component_container = {
+    width: {
+        xs: '98%',
+        sm: '98%',
+        md: '90%',
+        lg: '90%',
+    },
+    mx: 'auto',
+}
+
+// Top slider component style 
+export const boxInMap = {
+    position: 'relative',
+    height: {
+        xs: '150px',
+        sm: '150px',
+        md: '200px',
+    },
+}
+
+
--- src/pages/Home/TopSlider.jsx
+++ src/pages/Home/TopSlider.jsx
@@ -2,26 +2,17 @@ import React from 'react';
 import { Box, Grid, Typography } from '@mui/material';
 import { topSliderNewArrivalData } from '../../Utils/TopSliderNewArrivalData';
 import Banner from '../../components/Banner/Banner';
+import { boxInMap, component_container } from '../../Styles/ComponentStyle';
 
 function TopSlider() {
     return (
-        <Box 
-        style={{marginTop: '20px'}}
-        sx={{
-            width: {
-                xs: '98%',
-                sm: '98%',
-                md: '90%',
-                lg: '90%',
-            },
-            mx: 'auto',
-        }}>
+        <Box style={{ marginTop: '20px' }} sx={component_container}>
             <Grid container
                 rowSpacing={{ xs: 1, sm: 0, md: 3 }}
                 columnSpacing={{ xs: 0, sm: 0, md: 2 }}
                 sx={{ height: '80vh' }}
             >
-                <Grid item xs={12} sm={12} md={6} sx={{pt: 40}}>
+                <Grid item xs={12} sm={12} md={6} sx={{ pt: 40 }}>
                     <Banner />
                 </Grid>
                 <Grid item xs={12} sm={12} md={6}>
@@ -32,14 +23,7 @@ function TopSlider() {
                         {
                             topSliderNewArrivalData?.map((data, i) =>
                                 <Grid item xs={6}>
-                                    <Box sx={{
-                                        position: 'relative',
-                                        height: {
-                                            xs: '150px',
-                                            sm: '150px',
-                                            md: '200px',
-                                        },
-                                    }}
+                                    <Box sx={boxInMap}
                                     >
                                         <img src={data?.imageLink} className='rounded-md' alt="" />
                                         <Typography sx={{ position: 'absolute', top: 20, left: 20, color: 'red' }}>
