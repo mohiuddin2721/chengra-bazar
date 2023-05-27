a1d6aa4714f1d51bf722f740749d9d13cb6cdde7
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-05-27 17:56:33 +0600

    signIn page updated

--- src/Authentication/SignIn.jsx
+++ src/Authentication/SignIn.jsx
@@ -1,50 +1,84 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignIn.css';
 import SocialLogin from '../components/SocialAuthentication/SocialLogin';
+import { useForm } from 'react-hook-form';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const SignIn = () => {
-    const emailRef = useRef('');
+    const { register, handleSubmit } = useForm();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    // const emailRef = useRef('');
+    const onSubmit = data => console.log(data);
+
     const handleLogin = (event) => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
-        // console.log(email, password);
-        // console.log(data);
+
     }
 
-    const passwordReset =  (event) => {
-        const email = emailRef.current.value;
+    const passwordReset = (event) => {
+        // const email = emailRef.current.value;
         // console.log(email);
     }
 
     return (
-        <div className="login-container">
-            <div className="login-title">LOGIN</div>
-            <form className="login-form" onSubmit={handleLogin}>
-                <label htmlFor="">Your Email</label>
-                <input type="email" ref={emailRef} name='email' placeholder="Your Email" required />
-                <label htmlFor="">Your Password</label>
-                <input type="password" name='password' placeholder="password" required />
-                {/* {messageError} */}
-                <button type='submit'>Login</button>
-            </form>
-            <p className='mt-2'>
-                <small>New to chengra bazar?</small> 
-                <Link to="/signUp"
-                    className='text-[#140267] ml-2 text-sm font-bold'
+        <div className="login-page">
+            <div className="login-container">
+                <div className="login-title">LOGIN</div>
+                <form
+                    className="login-form"
+                    onSubmit={handleSubmit(onSubmit)}
                 >
-                    Please Sign Up
-                </Link>
-            </p>
-            <p
-                onClick={passwordReset}
-                className='text-[#140267] text-sm font-bold'
-            >
-                Forget Password?
-            </p>
-            <div>
-                <SocialLogin />
+                    <label>Your Email</label>
+                    <input
+                        type="email"
+                        placeholder="Your Email"
+                        {...register("email")}
+                    />
+
+                    <label>
+                        Your Password
+                        <span onClick={handleClickShowPassword}>
+                            {
+                                showPassword
+                                    ?
+                                    <RemoveRedEyeIcon className='text-[#70a2d4] ml-1' />
+                                    :
+                                    <VisibilityOffIcon className='text-[#70a2d4] ml-1' />
+                            }
+                        </span>
+                    </label>
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="password"
+                        {...register("password")}
+                    />
+                    <p
+                        onClick={passwordReset}
+                        className='text-[#140267] text-sm font-bold text-end underline cursor-pointer'
+                    >
+                        Forget Password
+                    </p>
+                    {/* {messageError} */}
+                    <button type='submit'>Login</button>
+                </form>
+                <p className='mt-2'>
+                    <small>Don't have an account?</small>
+                    <Link to="/signUp"
+                        className='text-[#140267] ml-2 text-sm font-bold'
+                    >
+                        Sign Up
+                    </Link>
+                </p>
+
+                <div>
+                    <SocialLogin />
+                </div>
             </div>
         </div>
     );
