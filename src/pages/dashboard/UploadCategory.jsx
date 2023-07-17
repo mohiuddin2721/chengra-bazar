bc823d259bd809da865553cf6e7ea4f6a5fdc3d9
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-07-17 17:43:51 +0400

    image hosted in imageBB successfully

---
+++ src/pages/dashboard/UploadCategory.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+
+const UploadCategory = () => {
+    const [selectedCategoryImage, setSelectedCategoryImage] = useState(null);
+
+    const imageHostKey = import.meta.env.VITE_REACT_APP_imageBBApiKey
+
+    function handleFileChange(event) {
+        setSelectedCategoryImage(event.target.files[0])
+    }
+
+    // console.log(selectedCategoryImage)
+
+    const handleCategory = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+
+        // formData.append('name', e.target.name?.value)
+        formData.append('image', selectedCategoryImage)
+        // Object.keys(categoryData).forEach(prop => {
+        //     formData.append(`${prop}`, formData[prop])
+        // })
+        const imageUploadUrl = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`
+        fetch(imageUploadUrl, {
+            method: "POST",
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgUrl => {
+                // console.log("imgData", imgUrl?.data?.url)
+                if (imgUrl.success) {
+                    const categoryData = {
+                        name: e.target.name?.value,
+                        photo: imgUrl?.data?.url,
+                    }
+                    const url = "http://localhost:5000/api/v1/products";
+                    fetch(imageUploadUrl, {
+                        method: "POST",
+                        body: formData
+                    })
+                        .then(res => res.json())
+                }
+                // if (posted.status === 'fail') {
+                //     Swal.fire({
+                //         icon: 'error',
+                //         title: 'Error',
+                //         text: 'Category is not inserted. please try again',
+                //     })
+                // } else {
+                //     Swal.fire({
+                //         icon: 'success',
+                //         title: 'Successful',
+                //         text: 'Category data Successfully updated',
+                //     })
+                // }
+                // setSelectedCategoryImage(null)
+                //         e.target.reset();
+            })
+
+        // console.log(selectedImg)
+        // console.log(categoryData)
+        // e.target.reset();
+        // setSelectedCategoryImage(null)
+    };
+
+    function deleteCategoryImage() {
+        setSelectedCategoryImage(null)
+    };
+
+    return (
+        <div>
+            <h2 className="text-lg font-semibold text-white capitalize">Add category</h2>
+            <form onSubmit={handleCategory}>
+                <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                    <div>
+                        <label className="text-white ">Product category</label>
+                        <input
+                            required
+                            name='name'
+                            type="text"
+                            placeholder="category name"
+                            className="w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-blue-500 focus:outline-none focus:ring"
+                        />
+                    </div>
+                    <div>
+                        <label className="text-white ">Upload just 1 photo</label>
+                        <input
+                            name='categoryPhoto'
+                            type="file"
+                            required
+                            onChange={handleFileChange}
+                            className="w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-blue-500 focus:outline-none focus:ring"
+                        />
+                    </div>
+                    <div>
+                        {
+                            selectedCategoryImage ?
+                                <div className='h-auto relative'>
+                                    <span
+                                        className='absolute left-1 top-0 text-2xl text-red-500 cursor-pointer'
+                                        onClick={deleteCategoryImage}
+                                    >
+                                        &times;
+                                    </span>
+                                    <img
+                                        className='w-[150px] max-h-[150px]'
+                                        src={URL.createObjectURL(selectedCategoryImage)}
+                                        alt="Category Image" />
+                                </div>
+                                :
+                                " "
+                        }
+                    </div>
+                </div>
+
+                <div className="flex justify-end mt-6">
+                    <button
+                        type='submit'
+                        className="px-6 py-2 text-white rounded-md transition-colors duration-200 transform bg-pink-500 hover:bg-pink-700"
+                    >
+                        Add category
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default UploadCategory;
--- src/pages/dashboard/Upload_Update.jsx
+++ src/pages/dashboard/Upload_Update.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { glassStyle_1, glassStyle_2 } from '../../Styles/DashboardStyle';
 import Swal from 'sweetalert2';
+import UploadCategory from './UploadCategory';
 
 const Upload_Update = () => {
     const [isHovered_1, setIsHovered_1] = useState(false);
@@ -16,6 +17,7 @@ const Upload_Update = () => {
     }
     function onFileSelect(event) {
         const files = event.target.files;
+        // console.log(files)
         if (files.length === 0 || files.length > 4) {
             return setImageWarning(warningText)
         };
@@ -24,11 +26,12 @@ const Upload_Update = () => {
             if (files[i]?.type.split('/')[0] !== 'image') continue;
             if (!images.some((e) => e.name === files[i].name)) {
                 setImages((prevImages) => [
-                    ...prevImages,
-                    {
-                        name: files[i].name,
-                        url: URL.createObjectURL(files[i]),
-                    },
+                    ...prevImages, files[i]
+                    // {
+                    //     name: files[i].name,
+                    //     // url: URL.createObjectURL(files[i]),
+                    //     url: files[i],
+                    // },
                 ]);
             }
         }
@@ -51,19 +54,21 @@ const Upload_Update = () => {
         event.preventDefault();
         serIsDragging(false);
         const files = event.dataTransfer.files;
+        // console.log(files)
         if (files.length === 0 || files.length > 4) {
             return setImageWarning(warningText)
         };
         for (let i = 0; i < 4; i++) {
             setImageWarning("")
-            if (files[i].type.split('/')[0] !== 'image') continue;
+            if (files[i]?.type.split('/')[0] !== 'image') continue;
             if (!images.some((e) => e.name === files[i].name)) {
                 setImages((prevImages) => [
-                    ...prevImages,
-                    {
-                        name: files[i].name,
-                        url: URL.createObjectURL(files[i]),
-                    },
+                    ...prevImages, files[i]
+                    // {
+                    //     name: files[i].name,
+                    //     // url: URL.createObjectURL(files[i]),
+                    //     url: files[i],
+                    // },
                 ]);
             }
         }
@@ -84,6 +89,13 @@ const Upload_Update = () => {
 
     const handleProductSubmit = (e) => {
         e.preventDefault();
+        const formData = new FormData();
+
+        // Update the formData object
+        // formData.append(
+        //     "image"
+        // );
+
         if (!images.length) {
             // return window.alert("Must upload maximum 4 photos")
             return Swal.fire({
@@ -92,8 +104,9 @@ const Upload_Update = () => {
                 text: 'Must upload maximum 4 photos',
             })
         }
-        const formData = {
-            name: e.target.unit.value,
+
+        const data = {
+            name: e.target.name.value,
             quantity: e.target.quantity.value,
             color: e.target.color.value,
             unit: e.target.unit.value,
@@ -103,34 +116,44 @@ const Upload_Update = () => {
             brand: e.target.brand.value,
             ratting: e.target.ratting.value,
             description: e.target.description.value,
-            imageUrl: images,
         }
-        console.log(formData)
-        e.target.reset();
-        setImages([])
+
+        images.forEach(prop => {
+            formData.append("image", prop)
+        })
+
+        Object.keys(data).forEach(prop => {
+            formData.append(`${prop}`, data[prop])
+        })
+
+        fetch("http://localhost:5000/api/v1/products", {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: formData
+        })
+            .then(res => res.json())
+            .then(posted => {
+                console.log("posted", posted)
+                if (posted.status === 'fail') {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'Product is not inserted. please try again',
+                    })
+                } else {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Successful',
+                        text: 'Product data Successfully updated',
+                    })
+                }
+                setImages([])
+                e.target.reset();
+            })
+        console.log("formdata", formData)
     };
-
-    const [selectedCategoryImage, setSelectedCategoryImage] = useState(null);
-    function handleFileChange(event) {
-        setSelectedCategoryImage(URL.createObjectURL(event.target.files[0]))
-    }
-
-    const handleCategory = (e) => {
-        e.preventDefault();
-        const categoryData = {
-            name: e.target.name?.value,
-            quantity: e.target.quantity?.value,
-            categoryPhoto: selectedCategoryImage,
-        }
-        console.log(categoryData)
-        e.target.reset();
-        setSelectedCategoryImage(null)
-    };
-    function deleteCategoryImage() {
-        setSelectedCategoryImage(null)
-    }
-
-
 
     return (
         <div>
@@ -283,7 +306,7 @@ const Upload_Update = () => {
                                                         </span>
                                                     </p>
                                                     <p className="text-xs text-white">
-                                                        PNG, JPG, JPEG up to 10MB
+                                                        PNG, JPG, JPEG up to 2MB
                                                     </p>
                                                 </>
                                             )}
@@ -316,7 +339,7 @@ const Upload_Update = () => {
                                     }
                                 </div>
                                 <div className='flex flex-wrap h-auto'>
-                                    {images?.map((images, index) => (
+                                    {images?.map((image, index) => (
                                         <div key={index} className='mr-2 mt-2 relative'>
                                             <span
                                                 className='absolute right-1 top-0 text-2xl cursor-pointer'
@@ -326,8 +349,9 @@ const Upload_Update = () => {
                                             </span>
                                             <img
                                                 className='w-[100px]'
-                                                src={images?.url}
-                                                alt={images?.name}
+                                                // src={URL.createObjectURL(images?.url)}
+                                                src={URL.createObjectURL(image)}
+                                                alt={image?.name}
                                             />
                                         </div>
                                     ))}
@@ -349,69 +373,7 @@ const Upload_Update = () => {
                 onMouseEnter={handleUploadHover_2}
                 onMouseLeave={handleUploadMouseLeave_2}
             >
-                <h2 className="text-lg font-semibold text-white capitalize">Add category</h2>
-                <form onSubmit={handleCategory}>
-                    <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-                        <div>
-                            <label className="text-white ">Product category</label>
-                            <input
-                                required
-                                name='name'
-                                type="text"
-                                placeholder="category name"
-                                className="w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-                            />
-                        </div>
-                        <div>
-                            <label className="text-white ">Quantity</label>
-                            <input
-                                required
-                                name='quantity'
-                                type="text"
-                                placeholder="Product quantity"
-                                className="w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-                            />
-                        </div>
-                        <div>
-                            <label className="text-white ">Upload just 1 photo</label>
-                            <input
-                                name='categoryPhoto'
-                                type="file"
-                                required
-                                onChange={handleFileChange}
-                                className="w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-                            />
-                        </div>
-                        <div>
-                            {
-                                selectedCategoryImage ?
-                                    <div className='h-auto relative'>
-                                        <span
-                                            className='absolute left-1 top-0 text-2xl text-red-500 cursor-pointer'
-                                            onClick={deleteCategoryImage}
-                                        >
-                                            &times;
-                                        </span>
-                                        <img
-                                            className='w-[150px] max-h-[150px]'
-                                            src={selectedCategoryImage}
-                                            alt="Category Image" />
-                                    </div>
-                                    :
-                                    " "
-                            }
-                        </div>
-                    </div>
-
-                    <div className="flex justify-end mt-6">
-                        <button
-                            type='submit'
-                            className="px-6 py-2 text-white rounded-md transition-colors duration-200 transform bg-pink-500 hover:bg-pink-700"
-                        >
-                            Add category
-                        </button>
-                    </div>
-                </form>
+                <UploadCategory />
             </section>
         </div>
     );
