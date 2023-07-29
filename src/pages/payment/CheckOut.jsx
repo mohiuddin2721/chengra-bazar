91cb63ada3711202443a9860ac0e3bcd6de6fbb2
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-07-30 01:19:41 +0400

    country code number implemented

--- src/pages/payment/CheckOut.jsx
+++ src/pages/payment/CheckOut.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddressForm from './AddressForm';
+import { BsFillPencilFill } from 'react-icons/bs';
 
 const CheckOut = () => {
+    const [zip, setZip] = useState(null)
+
+    const handleAddress = () => {
+        setZip(true)
+    }
+    const closeAddress = () => {
+        setZip(false);
+    }
+
     return (
         <div>
             <div>
-                <p className='text-white font-bold'>Check out page</p>
+                <p className='text-white font-bold'>Checkout</p>
+            </div>
+            <hr className='my-4' />
+            <div className='text-white w-full md:w-[60%] mx-auto block md:flex relative border-dashed hover:border-solid border-2 border-[#ffffffab] hover:border-green-400 rounded-lg p-8'>
+                <div className='w-full md:w-2/4 p-2'>
+                    <p className='font-bold text-xl'>1. Shipping address:</p>
+                </div>
+                <div className='w-full md:w-2/4 p-2 text-sm font-sans'>
+                    <p className="font-bold mb-2">Talib Ali (01680106149)</p>
+                    <p className="text-white">talibalidhfkh@gmail.com</p>
+                    <p className="text-white">Chengra Bazar (Zip: 015420)</p>
+                    <p className="text-white">Bangladesh, Dhaka</p>
+                </div>
+                <span
+                    onClick={handleAddress}
+                    className='absolute top-4 right-4 flex cursor-pointer text-green-500 hover:text-green-300 underline'>
+                    Change <BsFillPencilFill className='ml-2' />
+                </span>
+            </div>
+            <div className={zip ? 'block my-3 duration-300 ease-in-out opacity-100 transition-transform transform translate-y-0' : 'hidden'}>
+                <AddressForm closeAddress={closeAddress} />
             </div>
             <div>
-                <AddressForm />
+                <p className='text-white'>payment</p>
             </div>
         </div>
     );
