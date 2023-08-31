40c8279f811e6e7c080d98cc9bc4d836673a4794
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-08-31 17:17:57 +0400

    banner changed

--- src/Utils/ConstantData.jsx
+++ src/Utils/ConstantData.jsx
@@ -706,15 +706,15 @@ export const userDashMiniCardData = [
     },
 ]
 
-export const bannerPic = [
-    { id: 1, pic: 'https://as1.ftcdn.net/v2/jpg/01/91/82/44/1000_F_191824469_CJFxbuGqEJCnDRK83N97i17R7oEUJtEM.jpg' },
-    { id: 2, pic: 'https://as1.ftcdn.net/v2/jpg/02/19/10/12/1000_F_219101232_9gYB191gBUCxTSE455FUkmQB1MioqtVp.jpg' },
-    { id: 3, pic: 'https://as1.ftcdn.net/v2/jpg/01/23/12/38/1000_F_123123866_EGxIsP6jRf1hLP56OHDUWHHyJF2Kkhfp.jpg' },
-    { id: 4, pic: 'https://as1.ftcdn.net/v2/jpg/01/52/61/36/1000_F_152613619_kaNluqI3oUjvIhEQDcDfuksXknNJ45lf.jpg' },
-    { id: 5, pic: 'https://as1.ftcdn.net/v2/jpg/02/21/01/52/1000_F_221015223_JFpTeUN84cxi2SED9Ug9snZlifE0und4.jpg' },
-    { id: 6, pic: 'https://as2.ftcdn.net/v2/jpg/01/38/53/99/1000_F_138539919_c8vb5eC9112n4kxLBB4pU4ez6NC6G77i.jpg' },
-    { id: 7, pic: 'https://as2.ftcdn.net/v2/jpg/01/41/03/45/1000_F_141034518_wXdy3QxcL9vA1H5VY6Jl5PTNul4YLsE7.jpg' },
-]
+// export const bannerPic = [
+//     { id: 1, pic: 'https://as1.ftcdn.net/v2/jpg/01/91/82/44/1000_F_191824469_CJFxbuGqEJCnDRK83N97i17R7oEUJtEM.jpg' },
+//     { id: 2, pic: 'https://as1.ftcdn.net/v2/jpg/02/19/10/12/1000_F_219101232_9gYB191gBUCxTSE455FUkmQB1MioqtVp.jpg' },
+//     { id: 3, pic: 'https://as1.ftcdn.net/v2/jpg/01/23/12/38/1000_F_123123866_EGxIsP6jRf1hLP56OHDUWHHyJF2Kkhfp.jpg' },
+//     { id: 4, pic: 'https://as1.ftcdn.net/v2/jpg/01/52/61/36/1000_F_152613619_kaNluqI3oUjvIhEQDcDfuksXknNJ45lf.jpg' },
+//     { id: 5, pic: 'https://as1.ftcdn.net/v2/jpg/02/21/01/52/1000_F_221015223_JFpTeUN84cxi2SED9Ug9snZlifE0und4.jpg' },
+//     { id: 6, pic: 'https://as2.ftcdn.net/v2/jpg/01/38/53/99/1000_F_138539919_c8vb5eC9112n4kxLBB4pU4ez6NC6G77i.jpg' },
+//     { id: 7, pic: 'https://as2.ftcdn.net/v2/jpg/01/41/03/45/1000_F_141034518_wXdy3QxcL9vA1H5VY6Jl5PTNul4YLsE7.jpg' },
+// ]
 
 export const dashboardLink = {
     buyer: [
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,21 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
-import { bannerPic } from '../../Utils/ConstantData';
+import topbanner_1 from "../../assets/topbanner-1.jpg"
+import topbanner_2 from "../../assets/topbanner-2.jpg"
+import topbanner_3 from "../../assets/topbanner-3.jpg"
+import topbanner_4 from "../../assets/topbanner-4.jpg"
+import topbanner_5 from "../../assets/topbanner-5.jpg"
+import topbanner_6 from "../../assets/topbanner-6.jpg"
+
+const bannerPic = [
+    { id: 1, pic: topbanner_1 },
+    { id: 2, pic: topbanner_2 },
+    { id: 3, pic: topbanner_3 },
+    { id: 4, pic: topbanner_4 },
+    { id: 5, pic: topbanner_5 },
+    { id: 6, pic: topbanner_6 },
+]
 
 function Banner() {
     return (
