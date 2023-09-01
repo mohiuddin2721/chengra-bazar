6ef0af46a10da2498e84c050e0a1f4da026d3f39
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-09-01 16:56:02 +0400

    image add in customerCare

--- src/components/customerCare/CustomerCare.jsx
+++ src/components/customerCare/CustomerCare.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import customerServiceBg from '../../assets/custommerService.jpg'
 
 const CustomerCare = () => {
     return (
         <div className='min-h-[70vh]'>
-            <p>Customer care page</p>
+            <div className='w-full md:w-[96%] mx-auto'>
+                <img className='w-full' src={customerServiceBg} alt="" />
+            </div>
         </div>
     );
 };
